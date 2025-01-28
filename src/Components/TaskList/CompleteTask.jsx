e61a9dbabc526114c6c4b4f385545a0dba51380f
SubhPanda04 <subhasispanda1256700@gmail.com>
2025-01-29 01:12:06 +0530

    Added CompleteTask Functionality

---
+++ src/Components/TaskList/CompleteTask.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const CompleteTask = ({data}) => {
+  return (
+    <div className="flex-shrink-0 w-[300px] h-full px-4 py-7 bg-blue-400 rounded-xl">
+    <div className="flex items-end justify-between">
+      <h3 className="bg-red-500 py-1 px-3 rounded text-sm">{data.category}</h3>
+      <h4 className="text-sm">{data.taskDate}</h4>
+    </div>
+    <h2 className="mt-5 text-2xl font-semibold">{data.taskTitle}</h2>
+    <p className="text-sm mt-2">{data.taskDescription}</p>
+    <div className='flex justify-between mt-4'>
+      <button className='bg-red-500 py-1 px-1 text-sm cursor-pointer'>Complete</button>
+    </div>
+  </div>
+  )
+}
+
+export default CompleteTask
