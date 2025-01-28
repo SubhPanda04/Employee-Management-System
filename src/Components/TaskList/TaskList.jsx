739964aee48bf0f06f3f77be8106602c9c19e1a9
SubhPanda04 <subhasispanda1256700@gmail.com>
2025-01-29 01:13:57 +0530

    Updated TaskList to render tasks dynamically

--- src/Components/TaskList/TaskList.jsx
+++ src/Components/TaskList/TaskList.jsx
@@ -1,39 +1,28 @@
 import React from 'react'
+import AcceptTask from './AcceptTask'
+import NewTask from './NewTask'
+import CompleteTask from './CompleteTask'
+import FailedTask from './FailedTask'
 
-const TaskList = () => {
+const TaskList = ({data}) => {
   return (
-    <div id='tasklist' className='h-[55%] overflow-x-auto w-full py-5 mt-10 flex items-center justify-start gap-5 flex-nowrap'>
-        <div className='flex-shrink-0 w-[300px] h-full p-2 bg-red-400 rounded-xl'>
-            <div className='flex items-end justify-between'>
-                <h3 className='bg-red-500 py-1 px-3 ml-2 mt-2 rounded text-sm'>
-                    High
-                </h3>
-                <h4 className='mb-1 mr-1.5 text-sm'>25 Jan 2025</h4>
-            </div>
-            <h2 className='py-1 px-3 mt-5 text-2xl font-semibold'>Make a youtube video</h2>
-            <p className='text-sm py-1 px-3 mt-1'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque incidunt iure libero dolore, illo corporis!</p>
-        </div>
-        <div className='flex-shrink-0 w-[300px] h-full p-2 bg-blue-400 rounded-xl'>
-            <div className='flex items-end justify-between'>
-                <h3 className='bg-red-500 py-1 px-3 ml-2 mt-2 rounded text-sm'>
-                    High
-                </h3>
-                <h4 className='mb-1 mr-1.5 text-sm'>25 Jan 2025</h4>
-            </div>
-            <h2 className='py-1 px-3 mt-5 text-2xl font-semibold'>Make a youtube video</h2>
-            <p className='text-sm py-1 px-3 mt-1'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque incidunt iure libero dolore, illo corporis!</p>
-        </div>
-        <div className='flex-shrink-0 w-[300px] h-full p-2 bg-green-400 rounded-xl'>
-            <div className='flex items-end justify-between'>
-                <h3 className='bg-red-500 py-1 px-3 ml-2 mt-2 rounded text-sm'>
-                    High
-                </h3>
-                <h4 className='mb-1 mr-1.5 text-sm'>25 Jan 2025</h4>
-            </div>
-            <h2 className='py-1 px-3 mt-5 text-2xl font-semibold'>Make a youtube video</h2>
-            <p className='text-sm py-1 px-3 mt-1'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque incidunt iure libero dolore, illo corporis!</p>
-        </div>
-    </div>
+  <div id="tasklist" className="h-[50%] overflow-x-auto flex items-center justify-start gap-5 flex-nowrap w-full py-1 mt-16">
+      {data.tasks.map((element,idx)=>{
+          if(element.active) {
+            return <AcceptTask key={idx} data={element}/>
+          }
+          if(element.NewTask) {
+              return <NewTask key={idx} data={element}/>
+          }
+          if(element.completed) {
+            return <CompleteTask key={idx} data={element}/>
+          }
+          if(element.failed) {
+            return <FailedTask key={idx} data={element}/>
+          }
+      })}
+ </div>
+
   )
 }
 
